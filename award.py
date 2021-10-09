419e98ed9643836981114d4acaaaa50cf6c23949
JerryXRQ <55816134+JerryXRQ@users.noreply.github.com>
2021-10-09 00:27:12 -0500

    Minor Improvements

--- award.py
+++ award.py
@@ -3,13 +3,17 @@ import data
 import spacy
 import pandas as pd
 from collections import defaultdict
+import nltk
 
 def find_host(container):
     nlp = spacy.load("en_core_web_sm")
     candidate=defaultdict(int)
-    filter=set(["of","speech","score","dir","ever","dressed","nominee","i","we","alive","rubs","my","amazing","like","he","she","it","twitter","awkward","parts","show","insults","part"])
-    end_indicator=set(["award","goes",".","receive","golden","goldenglobes","category","wins","at","win","winner"])
+    filter=set(["buy","movie","hotel","friend","moments","of","speech","score","dir","ever","dressed","nominee","i","we","alive","rubs","my","amazing","like","he","she","it","twitter","awkward","parts","show","insults","part"])
+    end_indicator=set(["made","by","for","awarded",'-',"is","award","goes",".","receive","golden","goldenglobes","category","wins","at","win","winner"])
     counter=0
+    male_names = nltk.corpus.names.words('male.txt')
+    female_names = nltk.corpus.names.words('female.txt')
+    n=set(male_names+female_names)
     for ele in container.keys():
         m=container.get(ele)
         lis=m.get_text()
@@ -18,22 +22,44 @@ def find_host(container):
                 for j in range(i+1,len(lis)):
                     if lis[j] in filter:
                         break
-                    if lis[j] in end_indicator and j>i+1:
-                        extract=' '.join(lis[i:j])
+                    if lis[j][-1]=="-" and lis[j]!="-":
+                        target = lis[i:j+1]
+                        extract = ' '.join(target)
+                        extract = extract.replace("miniseries", "mini-series")
+                        extract = extract.replace("telecision", "television series")
+                        extract = extract.replace("seriestv", "television series")
+                        extract = extract.replace("tv", "television series")
+                        extract = extract.replace("series series", "series")
+                        extract = extract.replace("musical or comedy", "comedy or musical")
+                        candidate[extract] += 1
+                        counter += 1
+                        break
+                    if lis[j] in end_indicator or lis[j].capitalize() in n:
+                        if j<i+3:
+                            break
+                        target=lis[i:j]
+                        extract=' '.join(target)
                         extract=extract.replace("miniseries","mini-series")
-                        extract=extract.replace("seriestv","series television")
-                        extract = extract.replace("tv", "television")
-                        #print(extract)
+                        extract = extract.replace("telecision", "television series")
+                        extract=extract.replace("seriestv","television series")
+                        extract = extract.replace("tv", "television series")
+                        extract = extract.replace("series series", "series")
+                        extract = extract.replace("musical or comedy", "comedy or musical")
                         candidate[extract]+=1
                         counter+=1
                         break
+
+
+    keys=[k for k in candidate.keys()]
+    keys.sort(key=lambda x: candidate[x],reverse=True)
+    for i in range(25):
+        print(keys[i])
     for ele in candidate:
         if candidate[ele]>=20:
-            print(ele,candidate[ele])
+            return
+            #print(ele,candidate[ele])
 
     #print(candidate)
-
-
     return
 
 
