52b0cd8be0584fb3a0aabc46d0cadad052497db0
JerryXRQ <55816134+JerryXRQ@users.noreply.github.com>
2021-10-12 21:07:56 -0500

    Add Variable Search Scope

--- presenter.py
+++ presenter.py
@@ -5,16 +5,9 @@ import pandas as pd
 from collections import defaultdict
 import nltk
 import util
-
-def find_presenter(container,award):
-    nlp = spacy.load("en_core_web_sm")
-    candidate=defaultdict(int)
-    reduce=util.process_name(award)
-    #print(reduce)
-    key_word=set(["present","presented","presents","presenting"])
-    male_names = nltk.corpus.names.words('male.txt')
-    female_names = nltk.corpus.names.words('female.txt')
-    n=set(male_names+female_names)
+def narrow_search(container,award):
+    reduce = util.process_name(award)
+    key_word = set(["present", "presented", "presents", "presenting", "presenter","copresent"])
     selected=[]
     for ele in container.keys():
         m=container.get(ele)
@@ -23,7 +16,10 @@ def find_presenter(container,award):
         det1=True
         det2=False
         for words in reduce:
-            if words not in s:
+            if words=="tv":
+                if "tv" in s or "television" in s:
+                    continue
+            elif words not in s:
                 det1=False
                 break
         if not det1:
@@ -33,14 +29,54 @@ def find_presenter(container,award):
                 det2=True
         if det2:
             selected.append(lis)
+    return selected
+
+def broad_search(container,award):
+    reduce = util.expand_search(award)
+    key_word = set(["present", "presented", "presents", "presenting", "presenter", "copresent"])
+    selected = []
+    for ele in container.keys():
+        m = container.get(ele)
+        lis = m.get_text()
+        s = set(lis)
+        det1 = True
+        det2 = False
+        for words in reduce:
+            if words == "tv":
+                if "tv" in s or "television" in s:
+                    continue
+            elif words not in s:
+                det1 = False
+                break
+        if not det1:
+            continue
+        for kw in key_word:
+            if kw in s:
+                det2 = True
+        if det2:
+            selected.append(lis)
+    return selected
+
+
+def find_presenter(container,award):
+    nlp = spacy.load("en_core_web_sm")
+    candidate=defaultdict(int)
+    key_word = set(["present", "presented", "presents", "presenting", "presenter", "copresent"])
+    #print(reduce)
+
+    selected=narrow_search(container,award)
+    if len(selected)==0:
+        selected=broad_search(container,award)
+
     dic=defaultdict(int)
     for tweets in selected:
         sentence=""
-        print(tweets)
         for i in range(len(tweets)):
             if tweets[i] in key_word:
                 if i+1<len(tweets) and tweets[i+1]=="by":
                     sentence=" ".join(tweets[i+2:])
+                elif tweets[i]=="presenter":
+                    sentence=" ".join(tweets)
                 else:
                     sentence=" ".join(tweets[:i])
         doc=nlp(sentence)
@@ -48,9 +84,12 @@ def find_presenter(container,award):
             if ent.label_=="PERSON":
                 dic[ent.text]+=1
                 #print(ent.text)
+
     k=[k for k in dic.keys()]
     k.sort(key=lambda x:dic[x],reverse=True)
     #print(k)
+
+
     for j in range(min(2,len(k))):
        print(k[j])
     #print(candidate)
@@ -67,10 +106,37 @@ def main():
     run when grading. Do NOT change the name of this function or
     what it returns.'''
     # Your code here
+    OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama',
+                            'best performance by an actress in a motion picture - drama',
+                            'best performance by an actor in a motion picture - drama',
+                            'best motion picture - comedy or musical',
+                            'best performance by an actress in a motion picture - comedy or musical',
+                            'best performance by an actor in a motion picture - comedy or musical',
+                            'best animated feature film', 'best foreign language film',
+                            'best performance by an actress in a supporting role in a motion picture',
+                            'best performance by an actor in a supporting role in a motion picture',
+                            'best director - motion picture', 'best screenplay - motion picture',
+                            'best original score - motion picture', 'best original song - motion picture',
+                            'best television series - drama',
+                            'best performance by an actress in a television series - drama',
+                            'best performance by an actor in a television series - drama',
+                            'best television series - comedy or musical',
+                            'best performance by an actress in a television series - comedy or musical',
+                            'best performance by an actor in a television series - comedy or musical',
+                            'best mini-series or motion picture made for television',
+                            'best performance by an actress in a mini-series or motion picture made for television',
+                            'best performance by an actor in a mini-series or motion picture made for television',
+                            'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television',
+                            'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+
     file="gg2013.json"
     input=pd.read_json(file)
     c=data.container(input)
-    find_presenter(c, "best performance by an actress in a motion picture - drama")
+    #find_presenter(c,"best performance by an actor in a television series - comedy or musical")
+    #return
+    for ele in OFFICIAL_AWARDS_1315:
+        print(ele)
+        find_presenter(c, ele)
     print("Done")
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -6,19 +6,36 @@ from datetime import datetime
 
 def process_name(award_name):
     award_name.lower()
+    new = ""
+    filter = '[A-z|0-9| ]'
+    for characters in award_name:
+        if re.match(filter, characters):
+            new += characters
+    award_name=new
     name=award_name.replace(" in "," ")
     name=name.replace(" a "," ")
     name=name.replace(" by "," ")
     name=name.replace(" made for "," ")
-    name=name.replace("televison","tv")
+    name=name.replace("television","tv")
     name=name.replace(" an "," ")
     name=name.replace(" or "," ")
     name=name.replace(" - "," ")
-    name=name.replace("-","")
+    name=name.replace("-"," ")
+    name=name.replace(" role "," ")
+    name=name.replace(" miniseries "," ")
     name = name.replace(" performance ", " ")
     name = name.replace("best ", "")
     return name.split()
 
+def expand_search(award_name):
+    lis=process_name(award_name)
+    n=[]
+    for ele in lis:
+        if ele=="motion" or ele=="picture" or ele=="series":
+            continue
+        else:
+            n.append(ele)
+    return n
 
 def main():
     '''This function calls your program. Typing "python gg_api.py"
